a054861eef77058e1483d264743bd510b9aba481
Lakhan Mankani <lakhan.mankani@gmail.com>
2021-07-10 17:14:09 +0100

    Rename sntpTime to ntpTime
    Signed-off-by: Lakhan Mankani <lakhan.mankani@gmail.com>

--- client/ntptime.go
+++ client/ntptime.go
@@ -14,11 +14,11 @@ func NTPTimeFromByteArray(timeStamp []byte) NTPTime {
 	return NTPTime{binary.BigEndian.Uint32(timeStamp[0:4]), binary.BigEndian.Uint32(timeStamp[4:])}
 }
 
-func (sntpTime *NTPTime) Time() time.Time {
+func (ntpTime *NTPTime) Time() time.Time {
 	time1900 := time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC)
 
-	serverTime := time1900.Add(time.Second * time.Duration(sntpTime.seconds))
-	serverTime = serverTime.Add(time.Nanosecond * time.Duration(sntpTime.nanoSeconds))
+	serverTime := time1900.Add(time.Second * time.Duration(ntpTime.seconds))
+	serverTime = serverTime.Add(time.Nanosecond * time.Duration(ntpTime.nanoSeconds))
 
 	return serverTime
 }
@@ -29,9 +29,9 @@ func NTPTimeFromTime(t time.Time) NTPTime {
 	return NTPTime{uint32(timeDiff.Seconds()), uint32(timeDiff.Nanoseconds())}
 }
 
-func (sntpTime *NTPTime) ByteArrayFromNTP() []byte {
+func (ntpTime *NTPTime) ByteArrayFromNTP() []byte {
 	b := make([]byte, 8)
-	binary.BigEndian.PutUint32(b[0:4], sntpTime.seconds)
-	binary.BigEndian.PutUint32(b[4:8], sntpTime.nanoSeconds)
+	binary.BigEndian.PutUint32(b[0:4], ntpTime.seconds)
+	binary.BigEndian.PutUint32(b[4:8], ntpTime.nanoSeconds)
 	return b
 }
