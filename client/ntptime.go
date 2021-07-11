7d7c8ad4358529398b9347b6347cc54001a3b301
Lakhan Mankani <lakhan.mankani@gmail.com>
2021-07-11 13:40:27 +0100

    Fix fraction seconds calculation for NTPTime
    Signed-off-by: Lakhan Mankani <lakhan.mankani@gmail.com>

--- client/ntptime.go
+++ client/ntptime.go
@@ -2,36 +2,39 @@ package client
 
 import (
 	"encoding/binary"
+	"math"
 	"time"
 )
 
 type NTPTime struct {
-	seconds     uint32
-	nanoSeconds uint32
+	Seconds  uint32
+	Fraction uint32
 }
 
 func NTPTimeFromByteArray(timeStamp []byte) NTPTime {
-	return NTPTime{binary.BigEndian.Uint32(timeStamp[0:4]), binary.BigEndian.Uint32(timeStamp[4:])}
+	return NTPTime{binary.BigEndian.Uint32(timeStamp[0:4]), binary.BigEndian.Uint32(timeStamp[4:8])}
 }
 
 func NTPTimeFromTime(t time.Time) NTPTime {
 	time1900 := time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC)
 	timeDiff := t.Sub(time1900)
-	return NTPTime{uint32(timeDiff.Seconds()), uint32(timeDiff.Nanoseconds())}
+	seconds := timeDiff.Seconds()
+	fraction := (seconds - math.Trunc(seconds)) * math.MaxUint32
+	return NTPTime{uint32(seconds), uint32(fraction)}
 }
 
 func (ntpTime *NTPTime) Time() time.Time {
 	time1900 := time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC)
 
-	serverTime := time1900.Add(time.Second * time.Duration(ntpTime.seconds))
-	serverTime = serverTime.Add(time.Nanosecond * time.Duration(ntpTime.nanoSeconds))
+	serverTime := time1900.Add(time.Second * time.Duration(ntpTime.Seconds))
+	serverTime = serverTime.Add(time.Second * time.Duration(ntpTime.Fraction) / math.MaxUint32)
 
 	return serverTime
 }
 
 func (ntpTime *NTPTime) ByteArrayFromNTP() []byte {
 	b := make([]byte, 8)
-	binary.BigEndian.PutUint32(b[0:4], ntpTime.seconds)
-	binary.BigEndian.PutUint32(b[4:8], ntpTime.nanoSeconds)
+	binary.BigEndian.PutUint32(b[0:4], ntpTime.Seconds)
+	binary.BigEndian.PutUint32(b[4:8], ntpTime.Fraction)
 	return b
 }
